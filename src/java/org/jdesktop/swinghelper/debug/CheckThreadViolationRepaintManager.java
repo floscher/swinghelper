a735c75a5c94129a996664c225dca33a355914f3
alexfromsun <alexfromsun@231a8a49-c2fb-40e3-aa39-b2cf256393cc>
2006-09-26 09:17:58 +0000

    "imageUpdate" issue is fixed
    git-svn-id: https://svn.java.net/svn/swinghelper~svn/trunk@29 231a8a49-c2fb-40e3-aa39-b2cf256393cc

--- src/java/org/jdesktop/swinghelper/debug/CheckThreadViolationRepaintManager.java
+++ src/java/org/jdesktop/swinghelper/debug/CheckThreadViolationRepaintManager.java
@@ -4,23 +4,31 @@ import javax.swing.*;
 import java.lang.ref.WeakReference;
 
 /**
- * <p>This class is used to detect Event Dispatch Thread rule violations<br> 
+ * <p>This class is used to detect Event Dispatch Thread rule violations<br>
  * See <a href="http://java.sun.com/docs/books/tutorial/uiswing/misc/threads.html">How to Use Threads</a>
  * for more info</p>
- * 
+ * <p/>
  * <p>This is a modification of original idea of Scott Delap<br>
- * Initial version of ThreadCheckingRepaintManager can be found here<br>  
+ * Initial version of ThreadCheckingRepaintManager can be found here<br>
  * <a href="http://www.clientjava.com/blog/2004/08/20/1093059428000.html">Easily Find Swing Threading Mistakes</a>
  * </p>
- * 
+ *
  * @author Scott Delap
- * 
- * some improvements from Alexander Potochkin
+ *         <p/>
+ *         some improvements from Alexander Potochkin
  */
 public class CheckThreadViolationRepaintManager extends RepaintManager {
     // it is recommended to pass the complete check  
     private boolean completeCheck = true;
     private WeakReference<JComponent> lastComponent;
+
+    public CheckThreadViolationRepaintManager(boolean completeCheck) {
+        this.completeCheck = completeCheck;
+    }
+
+    public CheckThreadViolationRepaintManager() {
+        this(true);
+    }
 
     public boolean isCompleteCheck() {
         return completeCheck;
@@ -44,14 +52,23 @@ public class CheckThreadViolationRepaintManager extends RepaintManager {
         if (!SwingUtilities.isEventDispatchThread() && (completeCheck || c.isShowing())) {
             boolean repaint = false;
             boolean fromSwing = false;
+            boolean imageUpdate = false;
             StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
             for (StackTraceElement st : stackTrace) {
                 if (repaint && st.getClassName().startsWith("javax.swing.")) {
                     fromSwing = true;
                 }
+                if (repaint && "imageUpdate".equals(st.getMethodName())) {
+                    imageUpdate = true;
+                }
                 if ("repaint".equals(st.getMethodName())) {
                     repaint = true;
                 }
+            }
+            if (imageUpdate) {
+                //assuming it is java.awt.image.ImageObserver.imageUpdate(...) 
+                //image was asynchronously updated, that's ok 
+                return;
             }
             if (repaint && !fromSwing) {
                 //no problems here, since repaint() is thread safe
@@ -77,7 +94,7 @@ public class CheckThreadViolationRepaintManager extends RepaintManager {
         //Valid code  
         SwingUtilities.invokeAndWait(new Runnable() {
             public void run() {
-                test();
+                test();                
             }
         });
         System.out.println("Valid code passed...");
@@ -92,5 +109,18 @@ public class CheckThreadViolationRepaintManager extends RepaintManager {
         frame.pack();
         frame.setVisible(true);
         frame.dispose();
+    }
+
+    //this test must pass
+    static void imageUpdateTest() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JEditorPane editor = new JEditorPane();
+        frame.setContentPane(editor);
+        editor.setContentType("text/html");
+        //it works with no valid image as well 
+        editor.setText("<html><img src=\"file:\\lala.png\"></html>");
+        frame.setSize(300, 200);
+        frame.setVisible(true);
     }
 }
