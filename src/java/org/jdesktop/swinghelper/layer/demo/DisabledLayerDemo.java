a4b559119bc62a0c7859170cdeae2527fefe6e3f
alexfromsun <alexfromsun@231a8a49-c2fb-40e3-aa39-b2cf256393cc>
2007-06-14 13:02:07 +0000

    More painters added
    git-svn-id: https://svn.java.net/svn/swinghelper~svn/trunk@149 231a8a49-c2fb-40e3-aa39-b2cf256393cc

--- src/java/org/jdesktop/swinghelper/layer/demo/DisabledLayerDemo.java
+++ src/java/org/jdesktop/swinghelper/layer/demo/DisabledLayerDemo.java
@@ -1,9 +1,12 @@
 package org.jdesktop.swinghelper.layer.demo;
 
+import com.jhlabs.image.BlurFilter;
+import com.jhlabs.image.EmbossFilter;
 import org.jdesktop.swinghelper.layer.JXLayer;
 import org.jdesktop.swinghelper.layer.demo.util.LafMenu;
 import org.jdesktop.swinghelper.layer.effect.Effect;
 import org.jdesktop.swinghelper.layer.effect.ImageOpEffect;
+import org.jdesktop.swinghelper.layer.painter.AbstractPainter;
 import org.jdesktop.swinghelper.layer.painter.BufferedPainter;
 import org.jdesktop.swinghelper.layer.painter.DefaultPainter;
 import org.jdesktop.swinghelper.layer.painter.Painter;
@@ -11,8 +14,7 @@ import org.jdesktop.swinghelper.layer.painter.Painter;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
-import com.jhlabs.image.EmbossFilter;
+import java.awt.image.BufferedImageOp;
 
 /**
  * @author Alexander Potochkin
@@ -23,7 +25,9 @@ import com.jhlabs.image.EmbossFilter;
 public class DisabledLayerDemo extends JFrame {
     private JXLayer<JComponent> layer;
     private Painter<JComponent> simplePainter = new SimplePainter<JComponent>();
-    private Painter<JComponent> imageOpPainter = new ImageOpPainter<JComponent>();
+    private Painter<JComponent> translucentPainter = new TranslucentPainter<JComponent>();
+    private Painter<JComponent> blurPainter = new ImageOpPainter<JComponent>(new BlurFilter());
+    private Painter<JComponent> embossPainter = new ImageOpPainter<JComponent>(new EmbossFilter());
 
     private JCheckBoxMenuItem disablingMenuItem = new JCheckBoxMenuItem(new AbstractAction("Disable the layer") {
         public void actionPerformed(ActionEvent e) {
@@ -31,8 +35,10 @@ public class DisabledLayerDemo extends JFrame {
         }
     });
 
-    private JRadioButtonMenuItem simpleItem = new JRadioButtonMenuItem("Simple effect");
-    private JRadioButtonMenuItem imageOpItem = new JRadioButtonMenuItem("Emboss effect");
+    private JRadioButtonMenuItem titleItem = new JRadioButtonMenuItem("Title");
+    private JRadioButtonMenuItem translucentItem = new JRadioButtonMenuItem("Translucent color");
+    private JRadioButtonMenuItem blurItem = new JRadioButtonMenuItem("Blur effect");
+    private JRadioButtonMenuItem embossItem = new JRadioButtonMenuItem("Emboss effect");
 
     public DisabledLayerDemo() {
         super("Disabled/enabled layer demo");
@@ -42,7 +48,7 @@ public class DisabledLayerDemo extends JFrame {
         add(layer);
         add(createToolPanel(), BorderLayout.EAST);
         setJMenuBar(createMenuBar());
-        setSize(430, 250);
+        setSize(380, 300);
         setLocationRelativeTo(null);
     }
 
@@ -58,26 +64,43 @@ public class DisabledLayerDemo extends JFrame {
         menu.add(disablingMenuItem);
         menu.addSeparator();
 
+        titleItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, InputEvent.CTRL_MASK));
+        titleItem.setSelected(true);
+        menu.add(titleItem);
 
-        simpleItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, InputEvent.CTRL_MASK));
-        simpleItem.setSelected(true);
-        menu.add(simpleItem);
+        translucentItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_2, InputEvent.CTRL_MASK));
+        menu.add(translucentItem);
 
-        imageOpItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_2, InputEvent.CTRL_MASK));
-        menu.add(imageOpItem);
+        blurItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_3, InputEvent.CTRL_MASK));
+        menu.add(blurItem);
+
+        embossItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_4, InputEvent.CTRL_MASK));
+        menu.add(embossItem);
 
         ButtonGroup group = new ButtonGroup();
-        group.add(simpleItem);
-        group.add(imageOpItem);
+        group.add(titleItem);
+        group.add(translucentItem);
+        group.add(blurItem);
+        group.add(embossItem);
 
         ItemListener menuListener = new ItemListener() {
             public void itemStateChanged(ItemEvent e) {
-                layer.setPainter(simpleItem.isSelected() ? simplePainter : imageOpPainter);
+                if (titleItem.isSelected()) {
+                    layer.setPainter(simplePainter);
+                } else if (translucentItem.isSelected()) {
+                    layer.setPainter(translucentPainter);
+                } else if(blurItem.isSelected()) {
+                    layer.setPainter(blurPainter);
+                } else if (embossItem.isSelected()) {
+                    layer.setPainter(embossPainter);
+                }
             }
         };
 
-        simpleItem.addItemListener(menuListener);
-        imageOpItem.addItemListener(menuListener);
+        titleItem.addItemListener(menuListener);
+        translucentItem.addItemListener(menuListener);
+        blurItem.addItemListener(menuListener);
+        embossItem.addItemListener(menuListener);
 
         JMenuBar bar = new JMenuBar();
         bar.add(menu);
@@ -87,14 +110,17 @@ public class DisabledLayerDemo extends JFrame {
     }
 
     private JComponent createLayerPanel() {
-        JPanel panel = new JPanel();
+        JComponent panel = new JPanel();
         panel.add(new JCheckBox("JCheckBox"));
         panel.add(new JRadioButton("JRadioButton"));
-        JTextField textField = new JTextField("JTextField");
-        textField.setMaximumSize(new Dimension(100, 20));
-        panel.add(textField);
-        panel.add(new JScrollPane(new JTextArea(5, 20)));
+        panel.add(new JTextField(15));
         panel.add(new JButton("Have a nice day"));
+        panel.add(new JTextField(15));
+        panel.add(new JCheckBox("JCheckBox"));
+        panel.add(new JRadioButton("JRadioButton"));
+        panel.add(new JTextField(15));
+        panel.add(new JCheckBox("JCheckBox"));
+        panel.add(new JRadioButton("JRadioButton"));
         panel.setBorder(BorderFactory.createEtchedBorder());
         return panel;
     }
@@ -106,11 +132,17 @@ public class DisabledLayerDemo extends JFrame {
         box.add(Box.createGlue());
         box.add(button);
         box.add(Box.createGlue());
-        JRadioButton simple = new JRadioButton("Simple effect");
-        simple.setModel(simpleItem.getModel());
+        JRadioButton simple = new JRadioButton("Title");
+        simple.setModel(titleItem.getModel());
         box.add(simple);
+        JRadioButton translucent = new JRadioButton("Translucent");
+        translucent.setModel(translucentItem.getModel());
+        box.add(translucent);
+        JRadioButton blur = new JRadioButton("Blur effect");
+        blur.setModel(blurItem.getModel());
+        box.add(blur);
         JRadioButton emboss = new JRadioButton("Emboss effect");
-        emboss.setModel(imageOpItem.getModel());
+        emboss.setModel(embossItem.getModel());
         box.add(emboss);
         box.add(Box.createGlue());
         return box;
@@ -127,8 +159,22 @@ public class DisabledLayerDemo extends JFrame {
         }
     }
 
+    static class TranslucentPainter<V extends JComponent> extends AbstractPainter<V> {
+        public void paint(Graphics2D g2, JXLayer<V> l) {
+            l.paint(g2);
+            if (!l.isEnabled()) {
+                g2.setColor(new Color(0, 128, 128, 128));
+                g2.fillRect(0, 0, l.getWidth(), l.getHeight());
+            }
+        }
+    }
+
     static class ImageOpPainter<V extends JComponent> extends BufferedPainter<V> {
-        private Effect disablingEffect = new ImageOpEffect(new EmbossFilter());
+        private Effect disablingEffect;
+
+        public ImageOpPainter(BufferedImageOp disablingEffect) {
+            this.disablingEffect = new ImageOpEffect(disablingEffect);
+        }
 
         protected void paintToBuffer(Graphics2D g2, JXLayer<V> l) {
             super.paintToBuffer(g2, l);
